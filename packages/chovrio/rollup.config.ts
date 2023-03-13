365e127f97e60c8522dcf1a955a7a147c077afd5
chovrio <1480733667@qq.com>
2023-03-13 16:08:14 +0800

    feat: configure rollup

---
+++ packages/chovrio/rollup.config.ts
@@ -0,0 +1,22 @@
+import path from 'path'
+import { defineConfig } from 'rollup'
+import ts from '@rollup/plugin-typescript'
+import terser from '@rollup/plugin-terser'
+import { fileURLToPath } from 'url'
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+const cliConfig = defineConfig({
+  // 入口文件
+  input: './src/core/index.ts',
+  // 输出目录
+  output: {
+    file: path.resolve(__dirname, './dist/cli.js')
+  },
+  // 用到的插件
+  plugins: [
+    ts({ tsconfig: path.resolve(__dirname, './tsconfig.json') }),
+    terser({
+      toplevel: true
+    })
+  ]
+})
+export default defineConfig([cliConfig])
